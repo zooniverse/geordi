6d1b57d0bd0efbd02fe9494c9e5448d352e262c9
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-14 14:51:19 +0000

    testing node.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-function start() {
-  console.log("Request handler 'start' was called.");
-  return "Hello Start";
-}
+var http = require("http");
+var url = require("url");
 
-function upload() {
-  console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(handle, pathname, response);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
-exports.upload = upload;
