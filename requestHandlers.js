463fef447d57e338432d5251611a99f522349b3b
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-14 15:20:38 +0000

    testing node.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,13 @@
-var querystring = require("querystring");
-2
+var querystring = require("querystring"),
+    fs = require("fs");
+
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>' +
         '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
+        '<meta http-equiv="Content-Type" ' +
+        'content="text/html; charset=UTF-8" />' +
         '</head>' +
         '<body>' +
         '<form action="/upload" method="post">' +
@@ -29,5 +30,13 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response) {
+
+    console.log("Request handler 'show' was called.");
+    response.writeHead(200, {"Content-Type": "image/png"});
+    fs.createReadStream("/tmp/test.png").pipe(response);
+
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
