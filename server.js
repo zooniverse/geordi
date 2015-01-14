9a0e79c8044adc3c4919859a146901f747650757
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-14 15:26:40 +0000

    testing node.js

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ function start(route, handle) {
     console.log("Server has started.");
 }
 exports.start = start;
+
+function route(handle, pathname, response, request) {
+    console.log("About to route a request for " + pathname);
+    if (typeof handle[pathname] === 'function') {
+        handle[pathname](response, request);
+    } else {
+        console.log("No request handler found for " + pathname);
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("404 Not found");
+        response.end();
+    }
+}
+exports.route = route;
