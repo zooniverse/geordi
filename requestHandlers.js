5b1f05437d51de6b8078407f488a09b467fc13f6
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-15 11:08:12 +0000

    testing node.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -58,12 +58,12 @@ function list(response, request) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<table border='1' cellpadding='5' cellspacing='5'><th><td>ID</td><td>Time</td><td>Text</td></th>\n");
     db.find({}, function (err, docs) {
+        console.log('in callback');
         response.write('<p>Found '+docs.length+' docs.</p>');
         for (var doc in docs) {
             response.write("<tr><td>"+doc._id+"</td><td>"+doc.time+"</td><td>"+doc.text+"</td></tr>\n");
         }
     });
-    response.write('<p>Found '+docs.length+' docs.</p>');
     response.write("</table>\n");
     response.end();
 }
