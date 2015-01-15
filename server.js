1441c7efe9b0cd2fbc155eefc3e5926812a09eb1
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-15 10:10:21 +0000

    testing node.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var url = require("url");
 var querystring = require("querystring");
-var nedb = require("nedb")
+var nedb = require("nedb");
 
 function start(route, handle) {
     function onRequest(request, response) {
