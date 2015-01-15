2b0e26b0b9996b82ca77c116b32ac44db5a3f285
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-15 10:01:56 +0000

    testing node.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function route(handle, pathname, response, request) {
 exports.route = route;
 
 function routePost(handle, pathname, response, postData) {
-    console.log("About to route a request for " + pathname);
+    console.log("About to post route a request for " + pathname + " with postData of " + postData);
     if (typeof handle[pathname] === 'function') {
         handle[pathname](response, postData);
     } else {
