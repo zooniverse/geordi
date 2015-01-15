391add5efbab6eb59b14d0e2efa5a0f77719b1b5
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-15 14:53:50 +0000

    testing node.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var restify = require('restify');
-
-function respond(req, res, next) {
-  var doc = {name:req.params.name,data:12};
-  res.send(doc);
-  next();
-}
-
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
-
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+var fortune = require('fortune')
+  , app = fortune({
+    db: 'petstore'
+  })
+  .resource('person', {
+    name: String,
+    age: Number,
+    pets: ['pet'] // "has many" relationship to pets
+  })
+  .resource('pet', {
+    name: String,
+    age: Number,
+    owner: 'person' // "belongs to" relationship to a person
+  })
+  .listen(1337);
