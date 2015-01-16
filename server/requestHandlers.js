d03334bb86c1d9b0f1c64e177b50777f3f28cbc5
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-16 11:39:03 +0000

    CROWD-118: Building User Activity Logging Engine

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -1,6 +1,28 @@
 var utils = require('utils');
 var api = require('api_client');
 
+function start(response) {
+    console.log("Request handler 'start' was called.");
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" ' + 'content="text/html; charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/logEvent" method="post">' +
+        '<input type="text" name="user_id">' +
+        '<input type="text" name="subject_id">' +
+        '<input type="text" name="related_id">' +
+        '<input type="text" name="type">' +
+        '<input type="submit" value="Log Event">' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
 function logEvent(response, postData) {
     console.log("Request handler 'logEvent' was called.");
     var eventData = utils.getEventDataFromPost(postData);
