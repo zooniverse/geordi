3bd59290f38ebfab4b5f45062211680b3a4a04d3
Alex Bowyer <alex.bowyer@gmail.com>
2015-01-16 10:51:47 +0000

    CROWD-118: Building User Activity Logging Engine

--- server/tests/utils_tests.js
+++ server/tests/utils_tests.js
@@ -1,8 +1,16 @@
-// testing/nodeunit_basics.js
+var utils = require('utils');
+
 module.exports = {
     'Test getEventDataFromPost' : function(test) {
-        test.expect(1);
-        test.ok(true, "This shouldn't fail");
+        var parsed = utils.getEventDataFromPost("user_id=A123&subject_id=B456&type=classify&related_id=C789");
+        var today = Date.now().setHours(0, 0, 0, 0);
+        var dayOfTimeData = parsed.time.setHours(0,0,0,0);
+        test.expect(5);
+        test.ok(today==dayOfTimeData,"Event time should have been set to today's date.");
+        test.ok(parsed.user_id=="A123","User ID not set correctly.");
+        test.ok(parsed.subject_id=="B456","Subject ID not set correctly.");
+        test.ok(parsed.related_id=="C789","Related ID not set correctly.");
+        test.ok(parsed.type=="classify","Event Type not set correctly.");
         test.done();
     }
 };
