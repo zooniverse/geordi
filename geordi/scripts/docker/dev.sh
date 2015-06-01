b7890d53ba5aec794b9dfb13194577f49ee8ef22
Campbell Allen <campbell.allen@gmail.com>
2015-06-01 17:57:05 +0100

    docker-compose works

--- geordi/scripts/docker/dev.sh
+++ geordi/scripts/docker/dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -ex
 npm install
-ls -al /app/server/
-cp /app/config/docker/datasources.json /app/server/
+cp -f /geordi/config/docker/datasources.json /geordi/server/
 /usr/bin/supervisord
