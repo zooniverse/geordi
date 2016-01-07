68b7326836bc4cd6d06409856e9e734a344c2d0b
Alex Bowyer <alex.bowyer@gmail.com>
2016-01-07 10:16:23 +0000

    Add recording of client IP on every post event request.

--- geordi/common/models/event.js
+++ geordi/common/models/event.js
@@ -1,3 +1,10 @@
 module.exports = function(Event) {
-
+  // model operation hook
+  Event.observe('before save', function(ctx, next) {
+    if (ctx.instance) {
+      //ctx.instance.clientIP = (require("domain")).active.members[1].req.ip;
+      ctx.instance.clientIP = (require('loopback')).getCurrentContext().active.http.req.ip;
+    }
+    next();
+  });
 };
